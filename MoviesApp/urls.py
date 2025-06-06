125bbabd5d525d0018b7d78ae4dafacfaca00f25
usman-s-mahmood <usmanshahid027@outlook.com>
2025-06-06 17:32:56 +0500

    added toastr to the frontend, moving for code integration

--- MoviesApp/urls.py
+++ MoviesApp/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='movies-index'),
+    path('about', views.about, name='movies-about'),
+    path('search', views.search, name='movies-search'),
 ]
--- MoviesApp/views.py
+++ MoviesApp/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+    messages.success(
+        request,
+        'Testing Toast',
+        extra_tags='success'
+    )
     return render(
         request,
         'MoviesApp/index.html',
-        {}
+        {
+            'index': True
+        }
     )
+    
+def about(request):
+    return render(
+        request,
+        'MoviesApp/about.html',
+        {
+            'about': True
+        }
+    )
+    
+def search(request):
+    if request.method == 'POST':
+        pass
+    
